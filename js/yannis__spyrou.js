a2fd99ba228ab63830c43ecbf58f84d312424008
Iannes <info@yannisspyrou.co.uk>
2017-10-15 12:51:49 +0100

    Fix js link

--- js/yannis__spyrou.js
+++ js/yannis__spyrou.js
@@ -3,15 +3,13 @@ function small() {
 document.getElementById("shell").textContent="Digital Web Design";
 }
 
-if ( $(window).width() < 1200) {      
+if ( $(window).width() < 1200) {
   small();
-} 
+}
 
-console.log('If you are interested in my portfolio, please have a look at my online CV at https://yannisspyrou.co.uk/web-cv/')
+console.log('If you are interested in my portfolio, please have a look at my online CV at http://yannisspyrou.co.uk/cv.html/')
 
 
 var copy = document.getElementById('copy')
 var date =new Date()
-copy.textContent = date.getFullYear() 
-
-
+copy.textContent = date.getFullYear()
