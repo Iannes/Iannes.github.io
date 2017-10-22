d74eb465aef06d4edd2df1f867002e279b76bb05
Yannis Spyrou <js15js@gmail.com>
2017-10-22 16:57:17 +0100

    Upload bitcoin js

---
+++ js/bitcoin.js
@@ -0,0 +1,33 @@
+var btn = document.getElementById('btn')
+var price = document.getElementById('price')
+var url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+var currency = document.getElementById('currency')
+var value = ""
+
+function init() {
+  currencySelect()
+  document.getElementById('btn').click();
+
+}
+
+btn.addEventListener('click', function () {
+ var XHR = new XMLHttpRequest()
+
+XHR.onreadystatechange = function() {
+  if(XHR.readyState === 4 && XHR.status === 200) {
+    var data = JSON.parse(XHR.responseText).bpi
+    price.textContent = data[value].rate + " " +value
+  }
+
+}
+  XHR.open("GET", url)
+  XHR.send()
+})
+function currencySelect() {
+  value = currency.value;
+}
+
+
+var copy = document.getElementById('copy')
+var date =new Date()
+copy.textContent = date.getFullYear()
