e59e80e85a4820e7f8d37921024a6252ed86a2ea
Iannes <js15js@gmail.com>
2018-05-16 22:44:34 +0100

    feat:add js login fade in

--- js/signup.js
+++ js/signup.js
@@ -1,18 +1,28 @@
 const signUp        = document.querySelector('#signup')
 const signIn        = document.querySelector('#sign-in')
 const signInTrigger = document.querySelector('#sign-in--trigger')
+const signUpTrigger = document.querySelector('#sign-up--trigger')
 
 
 const show = () => {
   setTimeout(() => {
     signIn.classList.add('visible')
   }, 300);
+}
 
+const hide = () => {
+  setTimeout(() => {
+    signUp.classList.add('fade-in')
+  }, 100);
 }
 signInTrigger.addEventListener('click', () => {
   signUp.classList.add('fade-out')
   show()
 
+})
 
+signUpTrigger.addEventListener('click', () => {
+  signIn.classList.remove('visible')
+  hide()
 })
 
