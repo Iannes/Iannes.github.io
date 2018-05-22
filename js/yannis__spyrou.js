91f67e3d1274141c6ea19dcc914e45011b84034a
Iannes <js15js@gmail.com>
2018-05-22 22:32:34 +0100

    change title text on main site

--- js/yannis__spyrou.js
+++ js/yannis__spyrou.js
@@ -3,7 +3,7 @@ const copy = document.getElementById('copy'),
       win = window.innerWidth,
       shell = document.querySelector("#shell")
 
-const smallScreens = () => win < 1200 ? shell.textContent = "Welcome" : shell.textContent = "Web Development"
+const smallScreens = () => win < 1200 ? shell.textContent = "Welcome" : shell.textContent = "Bespoke Web Design"
 copy.textContent = date.getFullYear()
 
 smallScreens()
