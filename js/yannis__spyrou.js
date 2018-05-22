bebecbc0800135b16b1fd179e7e13de0143f6da6
Iannes <js15js@gmail.com>
2018-05-22 22:30:26 +0100

    restructure: js changes on main

--- js/yannis__spyrou.js
+++ js/yannis__spyrou.js
@@ -1,15 +1,11 @@
-let copy = document.getElementById('copy')
-let date = new Date()
-let win = window.innerWidth;
-const shell = document.querySelector("#shell")
+const copy = document.getElementById('copy'),
+      date = new Date(),
+      win = window.innerWidth,
+      shell = document.querySelector("#shell")
 
-const smallScreens = () => {
-  if (win < 1200) {
-  shell.textContent="Welcome";
-  }
-}
+const smallScreens = () => win < 1200 ? shell.textContent = "Welcome" : shell.textContent = "Web Development"
 copy.textContent = date.getFullYear()
 
 smallScreens()
 
-console.info('If you are interested in my portfolio, please contact me on js15js@gmail.com')
+console.info('If you are interested in my portfolio or CV, get in touch!')
